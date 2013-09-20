a45241ba17b1951cc537474f5451aef69088ae9c
SylvainBeg <begous@pc-a213-04.iut-valence.fr>
2013-09-20 10:01:24 +0200

    Added class Battle

---
+++ src/fr/iutvalence/java/mp/Battleship/Battle.java
@@ -0,0 +1,13 @@
+package fr.iutvalence.java.mp.Battleship;
+
+/**
+ * It's a session. That represents a new game.
+ * @author begous
+ *
+ */
+public class Battle
+{
+
+    public score scorePlayer;
+    public score scoreComputer;
+}
