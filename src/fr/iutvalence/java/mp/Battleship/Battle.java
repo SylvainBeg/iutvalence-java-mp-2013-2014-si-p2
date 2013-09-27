a9f408daf168a0b7ecb792b04ba2d4bb97f52685
SylvainBeg <begous@PC-A213-05.iut-valence.fr>
2013-09-27 09:25:07 +0200

    Delete class Score Update class Battle

--- src/fr/iutvalence/java/mp/Battleship/Battle.java
+++ src/fr/iutvalence/java/mp/Battleship/Battle.java
@@ -9,28 +9,40 @@ public class Battle
 {
     // FIXED TODO (think about it) does it makes sense to let the visibility as public ?
     /**
-     * Player 1 score
+     * Player 1 score ; number of ships sank at player 2 
      */
-    public Score scorePlayer1;
+    private int scorePlayer1;
 
     // FIXED TODO (think about it) does it makes sense to let the visibility as public ?
     // FIXED TODO (think about it) the game can only be played by an IA and a human ?
     /**
-     * Player 2 score : computer or human 
+     * Player 2 score ; number of ships sank at player 1 ; computer or human ?
      */
-    public Score scorePlayer2;
+    private int scorePlayer2;
     
     
     // TODO FIXED rename field
     /**
      * Number of turns
      */
-    public int nbTurn;
+    private int nbTurn;
     
     /**
      * Default number of battle 
      */
-    public final static int Battle_DEFAUT = 5;
+    private final static int Ships_DEFAULT = 5;
+    
+    /**
+     * Default length of grid
+     */
+    private final static int gridLength = 10;
+    
+    /**
+     * Player 1 grid
+     */
+    private int[][] player1Grid = new Area [Battle.gridLength][Battle.gridLength];
+    
+    
     
     // TODO (fix) initial field values should be set in constructors
     /**
@@ -38,24 +50,74 @@ public class Battle
      */
     public Battle()
     {
-        this.nbTour = 0;
-        this.scorePlayer1.nbShipAlive = Battle.Battle_DEFAUT;
-        this.scorePlayer1.nbShipDead = 0;
-        this.scorePlayer2.nbShipAlive = Battle.Battle_DEFAUT;
-        this.scorePlayer2.nbShipDead = 0;
+        this.nbTurn = 0;
+        this.scorePlayer1 = 0;
+        this.scorePlayer2 = 0;
+    }
+    
+    
+    
+    
+    /**
+     * return player 1 score
+     * @return player 1 score
+     */
+    public int getScorePlayer1()
+    {
+        return this.scorePlayer1;
     }
     
     /**
-     * Restart a game
+     * return player 2 score
+     * @return player 2 score
      */
-    public void restart()
+    public int getScorePlayer2()
     {
+        return this.scorePlayer2;
     }
     
     /**
-     * Exit a game
+     * return turn number
+     * @return turn number
      */
-    public void exit()
+    public int getNbTurn()
     {
+        return this.nbTurn;
     }
+    
+    /**
+     * Add a turn
+     */
+    public void updateTurn()
+    {
+        this.nbTurn = this.nbTurn +1;
+    }
+    
+    /**
+     * return ships default number, in order to calculate number of ships alive
+     * @return ships default number
+     */
+    public int getShips_DEFAUT()
+    {
+        return  Battle.Ships_DEFAULT;
+    }
+    
+    /**
+     * When the player 1 sink ship at player 2
+     */
+    public void updateScore1()
+    {
+        this.scorePlayer1 = this.scorePlayer1 +1;
+    }
+    
+    /**
+     * When the player 2 sink ship at player 1
+     */
+    public void updateScore2()
+    {
+        this.scorePlayer2 = this.scorePlayer2 +1;
+    }
+    
+    
+    
 }
